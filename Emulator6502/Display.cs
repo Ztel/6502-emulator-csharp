bea79540c9d4b56ea913bdc95065445b33f49cad
Dominic <30577026+Ztel@users.noreply.github.com>
2026-02-11 22:33:11 -0600

    generating and printing display based on memory buffers

---
+++ Emulator6502/Display.cs
@@ -0,0 +1,69 @@
+﻿using System.Text;
+
+namespace Emulator6502
+{
+    public class Display
+    {
+        ushort backgroundBufferAddress = 0x2000;
+        ushort spriteBufferAddress = 0x2100;
+
+        byte[] displayBuffer = new byte[256];
+        byte[] spriteBuffer = new byte[256];
+
+
+
+        public void ReadDisplayBuffers(byte[] memory)
+        {
+            for(int i  = 0; i < displayBuffer.Length; i++)
+            {
+                displayBuffer[i] = memory[backgroundBufferAddress + i];
+            }
+
+            for (int i = 0; i < spriteBuffer.Length; i++)
+            {
+                spriteBuffer[i] = memory[spriteBufferAddress + i];
+            }
+
+            //Overwrite background characters with sprite data. 
+            for (int i = 0; i < spriteBuffer.Length; i += 2)
+            {
+                //Each sprite is two adjacent bytes.
+                //First byte (i) is the position data, and the second byte is the ascii code
+                //Sprites only override the background layer if their code is NOT 0 (null) 
+                if (memory[spriteBufferAddress + i] != 0)
+                {
+                    displayBuffer[memory[spriteBufferAddress + i]] = memory[spriteBufferAddress + i + 1];
+                }
+            }
+        }
+
+        //Converts the display buffer into a 16x16 grid string (plus a border) to be printed to the console.
+        public string RenderDisplay()
+        {
+            StringBuilder displayBuilder = new StringBuilder();
+
+            displayBuilder.AppendLine("╔═════════════════════════════════╗");
+
+            for (int i = 0; i < 16; i++)
+            {
+                displayBuilder.Append("║ ");
+
+                for (int a = 0; a < 16; a++)
+                {
+                    //Replace ASCII control characters with spaces.
+                    char character = (displayBuffer[(i * 16) + a] > 31) ? (char)displayBuffer[(i * 16) + a] : (char)0x20;
+                    
+                    displayBuilder.Append(character + " ");
+                }
+
+                displayBuilder.AppendLine("║");
+            }
+
+            displayBuilder.AppendLine("╚═════════════════════════════════╝");
+
+            displayBuilder.AppendLine(DateTime.Now.ToLongTimeString());
+
+            return displayBuilder.ToString();
+        }
+    }
+}
--- Emulator6502/Emulator.cs
+++ Emulator6502/Emulator.cs
@@ -3,9 +3,17 @@
     public class Emulator
     {
         public CPU Cpu { get; set; } = new CPU();
+        public Display Screen { get; set; } = new Display();
+
+        private string displayString = "";
+
+        private int stepsPerFrame = 3200;
+        private int stepsUntilNewFrame;
 
         public bool stepMode = false;
         private bool executeProgram = false;
+
+
 
         public void LoadRom(string romFilePath)
         {
@@ -25,20 +33,35 @@
 
         public void StartProgram()
         {
-            ushort resetVector = (ushort)(Cpu.Memory[0xFFFD] * 256 + Cpu.Memory[0xFFFC]);
-
-            Cpu.ProgramCounter = resetVector;
             executeProgram = true;
+            Console.CursorVisible = false;
 
+            Cpu.Reset();
             RunProgram();
+        }
+
+        public void HaltProgram()
+        {
+            executeProgram = false;
+            Console.CursorVisible = true;
         }
 
         private void RunProgram()
         {
             while (executeProgram)
             {
-                Console.WriteLine("PC: $" + Cpu.ProgramCounter.ToString("X4") + "       A: $" + Cpu.Accumulator.ToString("X2") + "   X: $" + Cpu.XRegister.ToString("X2") + "   Y: $" + Cpu.YRegister.ToString("X2"));
                 Cpu.Step();
+                stepsUntilNewFrame--;
+
+                Console.SetCursorPosition(0, 0);
+                Console.Write("PC: $" + Cpu.ProgramCounter.ToString("X4") + "       A: $" + Cpu.Accumulator.ToString("X2") + "   X: $" + Cpu.XRegister.ToString("X2") + "   Y: $" + Cpu.YRegister.ToString("X2"));
+
+                if (stepsUntilNewFrame <= 0)
+                {
+                    UpdateScreen();
+
+                    stepsUntilNewFrame = stepsPerFrame;
+                }
 
                 if(stepMode)
                 {
@@ -47,9 +70,17 @@
             }
         }
 
-        public void HaltProgram()
+        private void UpdateScreen()
         {
-            executeProgram = false;
+            Console.SetCursorPosition(0, 0);
+            Console.WriteLine("PC: $" + Cpu.ProgramCounter.ToString("X4") + "       A: $" + Cpu.Accumulator.ToString("X2") + "   X: $" + Cpu.XRegister.ToString("X2") + "   Y: $" + Cpu.YRegister.ToString("X2"));
+
+            Screen.ReadDisplayBuffers(Cpu.Memory);
+            displayString = Screen.RenderDisplay();
+
+            Console.WriteLine(displayString);
+
+            Cpu.NMI();
         }
     }
 }
